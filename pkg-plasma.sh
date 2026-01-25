c95151a4de0b770efa2784a973b2848007d170dc
Franklin Souza <fraank@riseup.net>
2026-01-25 13:11:27 -0300

    Commit automático no repositório package-plasma!

--- pkg-plasma.sh
+++ pkg-plasma.sh
@@ -6,8 +6,35 @@
 
 
 # PACOTES INSTALL
-sudo pacman -S rustup --noconfirm && rustup default stable
-sudo pacman -S plasma-meta wayland xorg-xwayland kitty firefox-i18n-pt-br git wget pipewire pipewire-alsa pipewire-pulse \
-  pavucontrol mpv reflector dolphin xf86-video-amdgpu pacutils flatpak bitwarden \
-  noto-fonts noto-fonts-emoji noto-fonts-cjk ttf-fira-code ttf-fantasque-sans-mono ttf-jetbrains-mono-nerd ttf-jetbrains-mono \
-  dosfstools libreoffice-still-pt-br --noconfirm
+sudo pacman -S plasma-meta \
+  go \
+  npm \
+  yarn \
+  nodejs-lts-jod \
+  wayland \
+  xorg-xwayland \
+  kitty \
+  firefox-i18n-pt-br \
+  git \
+  wget \
+  pipewire \
+  pipewire-alsa \
+  pipewire-pulse \
+  pavucontrol \
+  mpv \
+  fastfetch \
+  reflector \
+  dolphin \
+  xf86-video-amdgpu \
+  pacutils \
+  flatpak \
+  bitwarden \
+  noto-fonts \
+  noto-fonts-emoji \
+  noto-fonts-cjk \
+  ttf-fira-code \
+  ttf-fantasque-sans-mono \
+  ttf-jetbrains-mono-nerd \
+  ttf-jetbrains-mono \
+  dosfstools \
+  libreoffice-still-pt-br --noconfirm
